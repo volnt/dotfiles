6afe1a13b2f78d959d74c441f6d38622abf2f25f
Florent Espanet <florent.esp@gmail.com>
2015-12-14 22:22:51 +0100

    Add awesome config linking.

--- link.sh
+++ link.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Emacs
 ln -s $PWD/.emacs $HOME/.emacs
+
+# Urxvt
 ln -s $PWD/.Xdefaults $HOME/.Xdefaults
 
+# Fish
 mkdir -p $HOME/.config/fish/functions/
 ln -s $PWD/fish/functions/*.fish $HOME/.config/fish/functions/
+
+# Awesome
+mkdir -p $HOME/.config/awesome/
+ln -s $PWD/awesome/* $HOME/.config/awesome/
